22abf28fba32aeb74064fc3628931105ea1feda2
Anwesh43 <anweshthecool0@gmail.com>
2018-10-03 01:07:07 +0530

    adding libraries and defining nodes

--- incompletetohalfarcview/src/main/java/com/anwesh/uiprojects/incompletetohalfarcview/IncompleteToHalfArcView.kt
+++ incompletetohalfarcview/src/main/java/com/anwesh/uiprojects/incompletetohalfarcview/IncompleteToHalfArcView.kt
@@ -3,3 +3,14 @@ package com.anwesh.uiprojects.incompletetohalfarcview
 /**
  * Created by anweshmishra on 03/10/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+
