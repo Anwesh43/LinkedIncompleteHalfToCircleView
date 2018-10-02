395deb845a300e8ce3200659a32643442f6f9a65
Anwesh43 <anweshthecool0@gmail.com>
2018-10-03 01:36:38 +0530

    addingg view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedincompletetohalfarcview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedincompletetohalfarcview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedincompletetohalfarcview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.incompletetohalfarcview.IncompleteToHalfArcView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        IncompleteToHalfArcView.create(this)
     }
 }
