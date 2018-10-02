ac712f03295736dfcbca6dd3c1dde4884c337dfb
Anwesh43 <anweshthecool0@gmail.com>
2018-10-03 01:24:30 +0530

    adding ITHANode class

--- incompletetohalfarcview/src/main/java/com/anwesh/uiprojects/incompletetohalfarcview/IncompleteToHalfArcView.kt
+++ incompletetohalfarcview/src/main/java/com/anwesh/uiprojects/incompletetohalfarcview/IncompleteToHalfArcView.kt
@@ -102,4 +102,47 @@ class IncompleteToHalfArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ITHANode(var i : Int, val state : State = State()) {
+        private var prev : ITHANode? = null
+        private var next : ITHANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = ITHANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawITHANODE(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ITHANode{
+            var curr : ITHANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
